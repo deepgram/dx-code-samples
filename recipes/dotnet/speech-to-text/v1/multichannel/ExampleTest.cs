e9218dab4549e7fb77bd2e81f513ffdec95dd144
Luke Oliff <luke@lukeoliff.com>
2026-04-02 22:43:05 +0100

    debug(dotnet): show dir/stderr/stdout in multichannel test failure

--- recipes/dotnet/speech-to-text/v1/multichannel/ExampleTest.cs
+++ recipes/dotnet/speech-to-text/v1/multichannel/ExampleTest.cs
@@ -22,7 +22,8 @@ public class ExampleTest
         var stderr = process.StandardError.ReadToEnd();
         process.WaitForExit(60_000);
 
-        Assert.Equal(0, process.ExitCode);
-        Assert.NotEmpty(stdout.Trim());
+        var debug = $"dir={dir}\nprojectDir={projectDir}\nexitCode={process.ExitCode}\nstdout={stdout}\nstderr={stderr}";
+        Assert.True(process.ExitCode == 0, debug);
+        Assert.True(stdout.Trim().Length > 0, debug);
     }
 }
